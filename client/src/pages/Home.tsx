e5da34545a2b9663fca3e51bdf6ceca1294fbf47
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2021-04-21 01:39:42 +0200

    Add: temporary sign in button to home

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,16 @@
 import { FC } from 'react';
+import { Button } from 'components';
+import { setRedirect } from 'helpers';
 
-export const Home: FC = () => (
-  <div>
-    home
-  </div>
-);
+export const Home: FC = () => {
+  function signIn() {
+    setRedirect('/workspace');
+    window.location.href = `https://feature-creep.auth.eu-west-1.amazoncognito.com/login?client_id=5mdqrbv4hchnh0v7dhh7isf2lb&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=${process.env.REACT_APP_HOSTNAME}/redirect`;
+  }
+
+  return (
+    <div>
+      <Button label="Sign in" onClick={signIn} />
+    </div>
+  );
+};
