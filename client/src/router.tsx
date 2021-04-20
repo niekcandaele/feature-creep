99952cacddc236f1b0d4d807e709e2287d67e1b7
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2021-04-21 01:43:45 +0200

    Create: simple routes

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,11 +1,22 @@
+import { AuthenticatedRoute } from 'components';
 import { FC } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Home } from './pages';
+import { Home, Workspace, Redirect, Frame, PageNotFound } from './pages';
 
 export const Router: FC = () => (
   <BrowserRouter>
     <Routes>
+
+      {/* Make sure that non authenticated routes are above /workspace */}
       <Route element={<Home />} path="/" />
+      <Route element={<Redirect />} path="/redirect" />
+
+      <AuthenticatedRoute element={<Frame />} path="/">
+        <Route element={<Workspace />} path="/workspace" />
+      </AuthenticatedRoute>
+
+      {/* Should remain the last route, since it matches with every path */}
+      <Route element={<PageNotFound />} path="*" />
     </Routes>
   </BrowserRouter>
 );
