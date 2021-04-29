a54813d796338106215287e8a66fd17463a36212
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2021-04-29 16:46:19 +0200

    Remove old icons component + add other components to general components index

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -1,6 +1,28 @@
 export { AuthenticatedRoute } from './AuthenticatedRoute';
+
+export { Avatar } from './Avatar';
+export type { AvatarProps } from './Avatar';
+
 export { Card } from './Card';
-export { Icon } from './icon';
+export type { CardProps } from './Card';
+
 export { Toggle } from './Toggle';
+export type { ToggleProps } from './Toggle';
+
 export { Link } from './Link';
-export { Button } from './buttons/Button';
+export type { LinkProps } from './Link';
+
+export { Button } from './Button';
+export type { ButtonProps } from './Button';
+
+export { Header } from './Header';
+
+export { Spinner } from './Spinner';
+
+export { SearchField } from './SearchField';
+export type { SearchFieldProps } from './SearchField';
+
+export { TextField } from './TextField';
+export type { TextFieldProps } from './TextField';
+
+export { Loading } from './Loading';
