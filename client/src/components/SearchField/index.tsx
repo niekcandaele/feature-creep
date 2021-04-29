87b710b442fd456af0913577ed841f990d85398c
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2021-04-29 16:41:40 +0200

    Create basic <SearchField/>

---
+++ client/src/components/SearchField/index.tsx
@@ -0,0 +1,45 @@
+import { FC } from 'react';
+import styled from 'styled';
+import { useController, Control, UseFormTrigger } from 'react-hook-form';
+import { AiOutlineSearch } from 'react-icons/ai';
+import debounce from 'debounce';
+
+const Container = styled.div`
+  position: relative;
+`;
+const SearchIcon = styled(AiOutlineSearch)`
+  position: absolute;
+  right: 10px;
+  top: 7px;
+  fill: ${({ theme }): string => theme.colors.secondary};
+`;
+const Input = styled.input`
+  padding-right: 35px;
+
+  &::placeholder {
+    font-weight: 600;
+  }
+`;
+
+export type SearchFieldProps = {
+  control: Control<any>
+  name: string; // unique name
+  placeholder?: string
+}
+
+export const SearchField: FC<SearchFieldProps> = ({ name, control, placeholder = 'Search' }) => {
+  const { field: { ref, ...inputProps } } = useController({ name, control, defaultValue: '' });
+
+  return (
+    <Container>
+      <Input
+        autoComplete="off"
+        type="text"
+        {...inputProps}
+        placeholder={placeholder}
+        ref={ref}
+      />
+      <SearchIcon size={24} />
+    </Container>
+  );
+};
