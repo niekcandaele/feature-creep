b486dc9cb281227c31bfb7cec3c7e97e6764c3e2
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2021-04-29 16:42:59 +0200

    Add simple <Spinner/> and <Loading/> components

---
+++ client/src/components/Loading/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+export const Loading: FC = () => {
+  return (
+    <div>loading page</div>
+  );
+};
---
+++ client/src/components/Spinner/index.tsx
@@ -0,0 +1,62 @@
+import { FC } from 'react';
+import { useTheme } from 'hooks';
+
+export const Spinner: FC = () => {
+  const theme = useTheme();
+  return (
+    <svg
+      height="44"
+      stroke={theme.colors.secondary}
+      viewBox="0 0 44 44"
+      width="44"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g fill="none" fillRule="evenodd" strokeWidth="2">
+        <circle cx="22" cy="22" r="1">
+          <animate
+            attributeName="r"
+            begin="0s"
+            calcMode="spline"
+            dur="1.8s"
+            keySplines="0.165, 0.84, 0.44, 1"
+            keyTimes="0; 1"
+            repeatCount="indefinite"
+            values="1; 20"
+          />
+          <animate
+            attributeName="stroke-opacity"
+            begin="0s"
+            calcMode="spline"
+            dur="1.8s"
+            keySplines="0.3, 0.61, 0.355, 1"
+            keyTimes="0; 1"
+            repeatCount="indefinite"
+            values="1; 0"
+          />
+        </circle>
+        <circle cx="22" cy="22" r="1">
+          <animate
+            attributeName="r"
+            begin="-0.9s"
+            calcMode="spline"
+            dur="1.8s"
+            keySplines="0.165, 0.84, 0.44, 1"
+            keyTimes="0; 1"
+            repeatCount="indefinite"
+            values="1; 20"
+          />
+          <animate
+            attributeName="stroke-opacity"
+            begin="-0.9s"
+            calcMode="spline"
+            dur="1.8s"
+            keySplines="0.3, 0.61, 0.355, 1"
+            keyTimes="0; 1"
+            repeatCount="indefinite"
+            values="1; 0"
+          />
+        </circle>
+      </g>
+    </svg>
+  );
+};
