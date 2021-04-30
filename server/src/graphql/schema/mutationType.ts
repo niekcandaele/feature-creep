553c4de227ce4ea65f2ba6fc844e1952dba72a02
niekcandaele <niek@candaele.dev>
2021-04-30 17:06:19 +0200

    feat: addMember and removeMember

--- server/src/graphql/schema/mutationType.ts
+++ server/src/graphql/schema/mutationType.ts
@@ -1,11 +1,15 @@
 import { GraphQLObjectType } from 'graphql';
 
 import { editPersonMutation } from './mutations/person/edit';
+import { addMemberToSquadMutation } from './mutations/squad/addMember';
+import { removeMemberFromSquadMutation } from './mutations/squad/removeMember';
 
 export const mutationType = new GraphQLObjectType({
   name: 'Mutation',
   //@ts-expect-error
   fields: () => ({
     editPerson: editPersonMutation,
+    addMemberToSquad: addMemberToSquadMutation,
+    removeMemberFromSquad: removeMemberFromSquadMutation,
   }),
 });
