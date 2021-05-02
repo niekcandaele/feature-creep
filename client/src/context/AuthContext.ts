e306519630b61dd8cc6fa1b09519d23bbcdee8ef
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2021-05-02 19:27:38 +0200

    Feat onboarding page (before merge)

--- client/src/context/AuthContext.ts
+++ client/src/context/AuthContext.ts
@@ -2,7 +2,7 @@ import { createContext } from 'react';
 import { setRedirect } from 'helpers/setRedirect';
 
 interface IAuthContext {
-  signIn: () => void;
+  signIn: (redirect: string) => void;
   signOut: () => boolean;
   isAuthenticated: () => Promise<boolean>
   register: () => void;
@@ -11,15 +11,16 @@ interface IAuthContext {
 export const AuthContext = createContext<IAuthContext>(authProvider());
 
 export function authProvider(): IAuthContext {
-  function signIn(path: string = '/workspace') {
-    setRedirect(path);
+  function signIn(redirect: string) {
+    setRedirect(redirect);
     window.location.href = `https://feature-creep.auth.eu-west-1.amazoncognito.com/login?client_id=5mdqrbv4hchnh0v7dhh7isf2lb&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=${process.env.REACT_APP_HOSTNAME}/redirect`;
     return;
   }
 
   function signOut(): boolean {
     // contains access token.
-    localStorage.removeItem('token');
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('idToken');
     return true;
   }
 
@@ -29,7 +30,7 @@ export function authProvider(): IAuthContext {
   }
   async function isAuthenticated(): Promise<boolean> {
     // Check if user has a token.
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem('accessToken');
     if (!token) { return false; }
 
     //    const { data, error } = await getClient().query({ query: GET_USER_DATA });
