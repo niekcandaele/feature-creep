e904b110573e324846f1ef0d3239069ad2203dd1
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2021-05-02 19:28:14 +0200

    Move type generate to script that does not run on start + Add onboarding to router

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,7 +1,8 @@
 import { AuthenticatedRoute } from 'components';
+import { CreateSquad } from 'pages/CreateSquad';
 import { FC } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Home, Workspace, Redirect, Frame, PageNotFound } from './pages';
+import { Home, Workspace, Redirect, Frame, PageNotFound, OnBoarding } from './pages';
 
 export const Router: FC = () => (
   <BrowserRouter>
@@ -9,9 +10,11 @@ export const Router: FC = () => (
       {/* Make sure that non authenticated routes are above /workspace */}
       <Route element={<Home />} path="/" />
       <Route element={<Redirect />} path="/redirect" />
+      <Route element={<OnBoarding />} path="/onboarding" />
 
       <AuthenticatedRoute element={<Frame />} path="/">
         <Route element={<Workspace />} path="/workspace" />
+        <Route element={<CreateSquad />} path="/create-squad" />
       </AuthenticatedRoute>
 
       {/* Should remain the last route, since it matches with every path */}
