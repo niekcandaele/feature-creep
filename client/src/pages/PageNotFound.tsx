ac5df1499b807ab27f0c3d5ec284151af9dccecb
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2021-05-02 19:28:38 +0200

    Template for createSquad page

--- client/src/pages/PageNotFound.tsx
+++ client/src/pages/PageNotFound.tsx
@@ -1,7 +1,22 @@
 import { FC } from 'react';
+import styled from 'styled';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 80vh;
+
+  p {
+    color: white;
+    font-size: 3rem;
+  }
+`;
 
 export const PageNotFound: FC = () => {
   return (
-    <div>page not found</div>
+    <Container>
+      <p>Page not found :(</p>
+    </Container>
   );
 };
