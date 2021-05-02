790388e695be18e3cb2789f397c943467d77c80d
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2021-05-02 19:25:04 +0200

    Create <SubPage/> template

---
+++ client/src/components/SubPage/index.tsx
@@ -0,0 +1,21 @@
+import { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Container, Title, ContentContainer } from './style';
+import { FaLongArrowAltLeft as ArrowLeft } from 'react-icons/fa';
+
+export interface SubPageProps {
+  title: string;
+}
+export const SubPage: FC<SubPageProps> = ({ title, children }) => {
+  const navigate = useNavigate();
+
+  return (
+    <Container>
+      {/* Goes back to the previous page. For some reason this is val -1*/}
+      <Title onClick={() => navigate(-1)}><ArrowLeft color="white" size={48} /><span>{title}</span></Title>
+      <ContentContainer>
+        {children}
+      </ContentContainer>
+    </Container>
+  );
+};
