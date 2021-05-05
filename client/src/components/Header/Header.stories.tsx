9888e21084166c3a0fe5c8d5fbaf97f600364f4e
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2021-05-05 19:29:14 +0200

    Remove template header story until apollo mock provider is implemented

--- client/src/components/Header/Header.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react/types-6-0';
-import { Header } from 'components';
-
-export default {
-  title: 'Components/Header',
-  component: Header,
-} as Meta;
-
-export const HeaderComponent: Story = () => <Header />;
