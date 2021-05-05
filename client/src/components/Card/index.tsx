809687a5d5a9271355c4f4417b8f91defa388b40
JariDevine <jari.de.wulf@student.howest.be>
2021-05-05 21:51:58 +0200

    Fix card component

--- client/src/components/Card/index.tsx
+++ client/src/components/Card/index.tsx
@@ -1,120 +1,43 @@
 import { FC } from 'react';
 import styled from 'styled';
-import { Link } from 'components';
+import { Link, useNavigate } from 'react-router-dom';
 
-const Template = styled.div`
-  border-radius: 1rem;
-  box-shadow: ${({ theme }): string => theme.shadow};
-  background-color: transparent;
-  border: 1px solid ${({ theme }): string => theme.colors.gray};
-  transition: transform 0.3s ease-in-out;
-  .text {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    h3 {
-      margin-bottom: 15px;
-    }
-  }
-  &:hover {
-    transform: translateY(-3px);
-  }
-`;
-const Small = styled(Template)`
-  padding: 30px 10px 30px 10px;
-  width: 200px;
-`;
-const Medium = styled(Template)`
-  padding: 30px 20px;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: flex-start;
-  width: 350px;
-  .text {
-    align-items: flex-start;
-    margin-left: 30px;
-    h3 {
-      margin-bottom: 5px;
-    }
-  }
-`;
-
-const Large = styled(Template)`
-  width: 400px;
-  padding: 20px 20px;
-  .title {
-    display: flex;
-    align-items: center;
-    margin-bottom: 20px;
-    h3 {
-      margin-left: 15px;
-      font-size: 2.225rem;
-    }
-  }
-  p {
-    margin-bottom: 15px;
-  }
-  p, a {
-    margin-left: 10px;
-  }
-`;
-
-const IconContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: ${({ theme }): string => theme.colors.gray};
+const Container = styled.div<{ isDisabled: boolean, isLink: boolean }>`
   width: fit-content;
-  padding: 5px;
-  border-radius: 50%;
-  margin: 0px auto 15px auto;
+  padding: 1rem 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  border: 2px solid ${({ theme }) => theme.colors.gray};
+  border-radius: 2rem;
+  transition: border-color .150s ease-in-out;
+  cursor: ${({ isLink }) => isLink ? 'pointer' : 'inherit'};
+  &:hover {
+    border-color: ${({ isDisabled, theme }) => isDisabled ? null : theme.colors.primary};
+  }
+  a{
+    display: inline;
+  }
 `;
 
 export interface CardProps {
-  title: string;
-  size: 'small' | 'medium' | 'large'
-  description: string;
   to?: string;
-  linkText?: string;
-  icon: React.ReactNode;
+  disabled?: boolean;
 }
 
-export const Card: FC<CardProps> = ({ title, description, icon, size, to, linkText }) => {
-  switch (size) {
-    case 'small':
-      return (
-        <Small>
-          <IconContainer>
-            {icon}
-          </IconContainer>
-          <div className="text">
-            <h3>{title}</h3>
-            <p>{description}</p>
-          </div>
-        </Small>
-      );
-    case 'medium':
-      return (
-        <Medium>
-          {icon}
-          <div className="text">
-            <h3>{title}</h3>
-            <p>{description}</p>
-          </div>
-        </Medium>
-      );
-    case 'large':
-      return (
-        <Large>
-          <div className="title">
-            {icon}
-            <h3>{title}</h3>
-          </div>
-          <p>{description}</p>
-          { linkText && to && <Link arrow to={to}><span>{linkText}</span></Link>}
-        </Large>
-      );
+export const Card: FC<CardProps> = ({ to, disabled = false, children }) => {
+  const navigate = useNavigate();
+  if (to) {
+    return (
+      <Container data-href={to} isDisabled={disabled} isLink onClick={() => navigate(to)} role="link">
+        {children}
+      </Container>
+    );
   }
+  return (
+    <Container isDisabled={disabled} isLink={false}>
+      {children}
+    </Container>
+  );
 };
--- client/src/pages/JoinSquad.tsx
+++ client/src/pages/JoinSquad.tsx
@@ -1,7 +1,40 @@
 import { FC } from 'react';
+import { useParams } from 'react-router-dom';
+import { gql, useQuery } from '@apollo/client';
+import { Squad } from 'generated';
+import { Spinner, SubPage } from 'components';
+
+
+const GET_USER_DATA = gql`
+query GET_USER_DATA {
+  person {
+    firstName
+    lastName
+    email
+  }
+}
+`;
 
 export const JoinSquad: FC = () => {
+  const loading = false;
+  const open = false;
+  const { id } = useParams();
+
+  //const { loading, data } = useQuery<{ squad: Squad }>(GET_USER_DATA, { variables: { id } });
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  if (open) {
+    return (
+      <div>Do you want to join Gryffindor?</div>
+    );
+  }
+
   return (
-    <div>join squad</div>
+    <SubPage title="Join a squad">
+      <div>Link expired, contact Gryffindor's squad leader.</div>
+    </SubPage>
   );
 };
