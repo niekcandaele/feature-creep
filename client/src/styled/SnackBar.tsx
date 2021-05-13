a26b8d7e47ab3b07b3df4d3162f8021266256aec
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2021-05-13 23:53:04 +0200

    Add warning snack

--- client/src/styled/SnackBar.tsx
+++ client/src/styled/SnackBar.tsx
@@ -1,12 +1,6 @@
 import { css } from 'styled-components';
 import { ThemeType } from './theme';
 
-/* TODO:
-  - Create story for textfield
-  - remove big create squad cards if user is part of a squad.
-  - Add squad dropdown to header.
-*/
-
 export const SnackBarStyles = css<{ theme: ThemeType }>`
   // Snackbar snack colors for every type
    #notistack-snackbar {
@@ -24,21 +18,26 @@ export const SnackBarStyles = css<{ theme: ThemeType }>`
     color: white;
     font-size: 1.5rem;
   }
-
-  div[class^="SnackbarItem-variantWarning-"], div[class*="SnackbarItem-variantWarning-"] {
-    background-color: ${({ theme }): string => theme.colors.error}!important;
+  // info
+  div[class^="SnackbarItem-variantInfo-"], div[class*="SnackbarItem-variantInfo-"] {
+    background-color: ${({ theme }): string => theme.colors.info}!important;
     color: white;
   }
+  // success
   div[class^="SnackbarItem-variantSuccess-"], div[class*="SnackbarItem-variantSuccess-"] {
     background-color: ${({ theme }): string => theme.colors.primary}!important;
     color: white;
   }
+  // warning
+  div[class^="SnackbarItem-variantWarning-"], div[class*="SnackbarItem-variantWarning-"] {
+    background-color: ${({ theme }): string => theme.colors.warning}!important;
+    color: white;
+  }
+  // error
   div[class^="SnackbarItem-variantError-"], div[class*="SnackbarItem-variantError-"] {
     background-color: ${({ theme }): string => theme.colors.error}!important;
     color: white;
   }
-  div[class^="SnackbarItem-variantInfo-"], div[class*="SnackbarItem-variantInfo-"] {
-    background-color: ${({ theme }): string => theme.colors.tertiary}!important;
-    color: white;
-  }
+
+
 `;
