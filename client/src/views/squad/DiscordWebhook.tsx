e743e8411e1d590cdc093c6a7594b6a6b0e0dc0c
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2021-05-14 18:01:01 +0200

    feat: users can add a discord webhook token

--- client/src/views/squad/DiscordWebhook.tsx
+++ client/src/views/squad/DiscordWebhook.tsx
@@ -1,36 +1,41 @@
 /* TODO: Rewrite this to more abstract form so other services can use this aswell. */
 
-import { Alert, TextField } from 'components';
+import { Alert, TextField, Button } from 'components';
 import { FC, useEffect, useMemo } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import * as yup from 'yup';
-import styled from 'styled-components';
+import styled from 'styled';
 import { RiDiscordFill as Discord } from 'react-icons/ri';
 import { useMutation, gql, useQuery } from '@apollo/client';
 import { useSnackbar } from 'notistack';
-import { Squad } from 'generated';
+import { SetNotificationConfigInput, Squad } from 'generated';
 import { useValidationSchema } from 'hooks';
 
 const Container = styled.div`
   width: 100%;
+  form button {
+    margin-left: auto;
+  }
 `;
 
 type FormFields = {
   token: string;
 }
 
-const SET_DISCORD_TOKEN = gql`
-  mutation SET_DISCORD_TOKEN {
-    setSquadOpen(id: { squadId: "123"}){
-      id
+const GET_EXISTING_TOKEN = gql`
+  query GET_EXISTING_TOKEN($id: String!){
+    squad(id: $id){
+      notificationConfig {
+        discordWebhook
+      }
     }
   }
 `;
 
-const GET_EXISTING_TOKEN = gql`
-  query GET_EXISTING_TOKEN($id: String!){
-    squad(id: $id){
-      token
+const SET_DISCORD_TOKEN = gql`
+  mutation SET_DISCORD_TOKEN($input: SetNotificationConfigInput) {
+    setNotificationConfig(input: $input){
+      id
     }
   }
 `;
@@ -39,9 +44,11 @@ interface DiscordWebhookProps {
   squadId: string;
 }
 export const DiscordWebhook: FC<DiscordWebhookProps> = ({ squadId }) => {
+  const webhookRegex = /discord.com\/api\/webhooks\/([^\/]+)\/([^\/]+)/;
+
   const validationSchema = useMemo(
     () => yup.object({
-      token: yup.string()
+      token: yup.string().matches(webhookRegex, 'This is not a valid webhook format.').required('This field is required')
     }), []
   );
 
@@ -52,23 +59,22 @@ export const DiscordWebhook: FC<DiscordWebhookProps> = ({ squadId }) => {
   });
   const { enqueueSnackbar } = useSnackbar();
   const { loading: getTokenLoading, data: getTokenData, error: getTokenError } = useQuery<{ squad: Squad }>(GET_EXISTING_TOKEN, { variables: { id: squadId } });
-  const [setToken, { called, data: setTokenData, loading: setTokenLoading, error: setTokenError }] = useMutation<{ squad: Squad }, { token: string }>(SET_DISCORD_TOKEN);
+  const [setToken, { called, data: setTokenData, loading: setTokenLoading, error: setTokenError }] = useMutation<{ squad: Squad }, { input: SetNotificationConfigInput }>(SET_DISCORD_TOKEN);
 
   const onSubmit: SubmitHandler<FormFields> = ({ token }) => {
-    setToken({ variables: { token } });
+    setToken({ variables: { input: { discordWebhook: token, squadId: squadId } } });
   };
 
   useEffect(() => {
     if (called && setTokenData) {
       // TODO: check if token is set based on returned data.
-      enqueueSnackbar('Successfully set the new discord token!', { variant: 'success' });
+      enqueueSnackbar('Successfully set the new discord token! You should have received a test message in one of your Discord channels.', { variant: 'success' });
     }
   }, [setTokenData]);
 
   useEffect(() => {
-    // TODO: set this to token instead of id
-    if (getTokenData && getTokenData.squad && getTokenData.squad.id) {
-      setValue('token', getTokenData.squad.id);
+    if (getTokenData && getTokenData.squad && getTokenData.squad.notificationConfig?.discordWebhook) {
+      setValue('token', getTokenData.squad.notificationConfig?.discordWebhook);
     }
   }, [getTokenData]);
 
@@ -87,6 +93,12 @@ export const DiscordWebhook: FC<DiscordWebhookProps> = ({ squadId }) => {
           loading={getTokenLoading || setTokenLoading}
           name="token"
           placeholder="place webhook URL here"
+        />
+        <Button
+          isLoading={setTokenLoading}
+          onClick={() => { /* dummy */ }}
+          text="Save token"
+          type="submit"
         />
       </form>
       <Alert
