63937ec041b4e2bd2d645e820518702c9f3230d3
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2021-05-14 14:15:08 +0200

    Switch to new avatar

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import { Container, ContentContainer, Ghost, Nav, User, List } from './style';
 import { Avatar, Button, Link, MenuDropDown } from 'components';
 import { useUser } from 'hooks';
-import templatePerson from 'images/person.jpeg';
+import backup from 'images/avatars/avatar-03.png';
 import create from 'images/emoji/create.png';
 
 export const Header: FC = () => {
@@ -30,7 +30,7 @@ export const Header: FC = () => {
         </Nav>
         <Button onClick={createSession} size="large" text="Start Session" />
         <User>
-          <Avatar size="small" src={templatePerson} />
+          <Avatar size="small" src={userData.avatar ? userData.avatar : backup} />
           <span>{userData.firstName} {userData.lastName}</span>
         </User>
       </ContentContainer>
