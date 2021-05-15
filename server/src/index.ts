deaa92a4a233792bcc40df97d8bcc4bc2b0faa1f
niekcandaele <niek@candaele.dev>
2021-05-15 16:30:56 +0200

    fix: load gears client on app boot

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import { config } from 'dotenv';
 
+import { GearsClient } from './gears/gears';
 import { server } from './graphql';
 import { getRediSearch } from './redisearch/client';
 
@@ -7,6 +8,8 @@ config();
 
 async function main() {
   const search = await getRediSearch();
+  const gears = new GearsClient();
+  await gears.initialize();
 
   server.listen().then((data: any) => {
     console.log(`🚀  Server ready at ${data.url}`);
